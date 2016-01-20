3b2f438e2ded672868b12af3b603c6d5df892df7
Grant Miner <xxgsoftware@gmail.com>
2016-01-19 20:47:35 -0600

    Reindent and refactor a bit

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 'use strict';
-const adapt = require('koa-adapter'); // adapt pre Koa 2.0 middle ware to be compatible with Koa 2.0.
 const Promise = require('bluebird');
-const Koa = require('koa');
-const app = module.exports = new Koa();
-const session = require('koa-session');
-const compress = require('koa-compress')();
-const helmet = require('koa-helmet');
-const favicon = require('koa-favicon');
-const conditional = require('koa-conditional-get');
-const etag = require('koa-etag');
-const morgan = require('koa-morgan')();
-const bodyParser = require('koa-bodyparser')();
-const serveStatic = require('koa-serve-static')('public');
 const cluster = require('cluster');
+
+// middleware
+const serveStatic = require('koa-serve-static')('public');
+const conditional = require('koa-conditional-get');
+const bodyParser = require('koa-bodyparser')();
+const compress = require('koa-compress')();
+const morgan = require('koa-morgan')();
+const favicon = require('koa-favicon');
+const session = require('koa-session');
+const adapt = require('koa-adapter'); // adapt pre Koa 2.0 middle ware to be compatible with Koa 2.0.
+const helmet = require('koa-helmet');
+const etag = require('koa-etag');
+
+const Koa = require('koa');
+
+const app = module.exports = new Koa();
 
 app.use(adapt(favicon(__dirname + '/public/favicon.ico')));
 app.use(adapt(require('koa-response-time')()));
@@ -21,47 +25,47 @@ app.use(adapt(etag()));
 app.use(morgan);
 
 app.use(adapt(compress));
-app.keys = '123';
+app.keys = 'put a random string here';
 
 app.use(adapt(session({
-	maxAge: 9999999999000
+    maxAge: 9999999999000
 }, app)));
 
 app.use(adapt(bodyParser));
 
 // Example error handler to JSON stringify errors
-app.use(adapt(function *(next){
-  try {
-    yield next;
-  } catch (err) {
-    // some errors will have .status
-    // however this is not a guarantee
-    this.status = err.status || 500;
-    this.type = 'application/json';
-    this.body = JSON.stringify({
-		success: false,
-		message: err.stack
-	})
+app.use(adapt(function*(next) {
+    try {
+        yield next;
+    } catch (err) {
+        // some errors will have .status
+        // however this is not a guarantee
+        this.status = err.status || 500;
+        this.type = 'application/json';
+        this.body = JSON.stringify({
+            success: false,
+            message: err.stack
+        })
 
-    // since we handled this manually we'll
-    // want to delegate to the regular app
-    // level error handling as well so that
-    // centralized still functions correctly.
-    this.app.emit('error', err, this);
-  }
+        // since we handled this manually we'll
+        // want to delegate to the regular app
+        // level error handling as well so that
+        // centralized still functions correctly.
+        this.app.emit('error', err, this);
+    }
 }));
 
 const router = require('koa-router')();
 
-router.get('/', function *(next) {
-	this.status = 200;
-	this.body = 'Hello world from worker ' + (cluster.worker ? cluster.worker.id : '') + '!';
+router.get('/', function*(next) {
+    this.status = 200;
+    this.body = 'Hello world from worker ' + (cluster.worker ? cluster.worker.id : '') + '!';
 })
 
-router.get('/api/example', function *(next) {
-	this.response.body = yield Promise.delay(3000).then(function () {
-		return "Simple Async 3-second Delayed Example!";
-	})
+router.get('/api/example', function*(next) {
+    yield Promise.delay(3000);
+
+    this.response.body = "Simple Async 3-second Delayed Example!";
 })
 
 app.use(adapt(router.routes()));
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 console.log('Starting TLS server at ' + (new Date).toISOString());
-process.on('exit', function () {
-	console.log('Process exit at ' + (new Date).toISOString());
+process.on('exit', function() {
+    console.log('Process exit at ' + (new Date).toISOString());
 });
 
 const Promise = require('bluebird');
@@ -11,48 +11,57 @@ const cluster = require('cluster');
 const pem = Promise.promisifyAll(require('pem'));
 const app = require('./app');
 const config = require('./config.json');
+const spdy = require('spdy');
+const socketIo = require('socket.io')
 
 if (process.getuid() === 0) { // if we are root
-	var port = 443;
+    var port = 443;
 } else { // we are not root, can only use sockets >1024
-	var port = 8443;
+    var port = 8443;
 }
 
-pem.createCertificateAsync({days:1, selfSigned:true}) // generate a cert/keypair on the fly
-.then(function (keys) {
-	const credentials = {key: keys.serviceKey, cert: keys.certificate};
+Promise.coroutine(function*() { // same as an async function; allows use of yield to await promises.
+    const keys = yield pem.createCertificateAsync({
+        days: 1,
+        selfSigned: true
+    }); // generate a cert/keypair on the fly
 
-	function getServer() {
-		const server = require('spdy').createServer(credentials, app.callback());
-		const io = require('socket.io').listen(server);
+    const credentials = {
+        key: keys.serviceKey,
+        cert: keys.certificate
+    };
 
-	    io.on('connection', function (socket) {
-	        // TODO: do stuff with socket
-	    })
+    function getServer() {
+        const server = spdy.createServer(credentials, app.callback());
+        const io = socketIo.listen(server);
 
-		return server;
-	}
+        io.on('connection', function(socket) {
+            // TODO: do stuff with socket
+        })
 
-	if (config.cluster) {
-		sticky(function () {
-			return getServer();
-		}).listen(port, function () {
-			console.log('Cluster worker ' + (cluster.worker ? cluster.worker.id : '') + ' HTTPS server listening on port ' + port);
-		});
-	} else {
-		getServer().listen(port, function () {
-			console.log('HTTPS server (no cluster) listening on port ' + port);
-		});
-	}
+        return server;
+    }
 
-	if (process.getuid() === 0) { // if we are root
-		// we have opened the sockets, now drop our root privileges
-		process.setgid('nobody');
-		process.setuid('nobody');
-		// Newer node versions allow you to set the effective uid/gid
-		if (process.setegid) {
-			process.setegid('nobody');
-			process.seteuid('nobody');
-		}
-	}
-})
+    if (config.cluster) {
+        sticky(function() {
+            return getServer();
+        }).listen(port, function() {
+            console.log('Cluster worker ' + (cluster.worker ? cluster.worker.id : '') + ' HTTPS server listening on port ' + port);
+        });
+    } else {
+        getServer().listen(port, function() {
+            console.log('HTTPS server (no cluster) listening on port ' + port);
+        });
+    }
+
+    if (process.getuid() === 0) { // if we are root
+        // we have opened the sockets, now drop our root privileges
+        process.setgid('nobody');
+        process.setuid('nobody');
+        // Newer node versions allow you to set the effective uid/gid
+        if (process.setegid) {
+            process.setegid('nobody');
+            process.seteuid('nobody');
+        }
+    }
+})();
