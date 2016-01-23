7cfd73b3d7ea1afec7176a3113741d4d051e3e97
Grant Miner <xxgsoftware@gmail.com>
2016-01-22 18:00:52 -0600

    Tidy up

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ process.on('exit', function() {
 
 const Promise = require('bluebird');
 const sticky = require('socketio-sticky-session');
-const fs = require('fs');
 const cluster = require('cluster');
 const pem = Promise.promisifyAll(require('pem'));
 const app = require('./app');
 const config = require('./config.json');
 const spdy = require('spdy');
 const socketIo = require('socket.io')
+const os = require('os');
 
 if (process.getuid() === 0) { // if we are root
     var port = 443;
@@ -43,9 +43,11 @@ Promise.coroutine(function*() { // same as an async function; allows use of yiel
     }
 
     if (config.cluster) {
-        sticky(function() {
-            return getServer();
-        }).listen(port, function() {
+        sticky({
+            // https://github.com/wzrdtales/socket-io-sticky-session
+            num: os.cpus(), // process count
+            proxy: false, // if the layer 4 patching should be used or not, needed if behind a proxy.
+        }, getServer).listen(port, function() {
             console.log('Cluster worker ' + (cluster.worker ? cluster.worker.id : '') + ' HTTPS server listening on port ' + port);
         });
     } else {
